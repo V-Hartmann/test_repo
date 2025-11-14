0089240d87e4a321f0257a4f7490fa34432597f9
V-Hartmann <raphael.danilov@gmail.com>
2025-11-14 11:36:27 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
+# Your first line of Python code
 import json
 
-def handle(n):
-    empty_list = []
-    a, b = 0, 1
-    while a < 1000:
-        a, b = b, a+b
-        empty_list.append([a, b])
-    return json.dumps({"list":empty_list, "input": n})
+def handle(input_data):
+    input_data = json.loads(input_data)
+    
+    return json.dump(input_data)
