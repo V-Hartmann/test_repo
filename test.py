0fa29c4c681db1811b89b5b6c04ea137f0b8c439
V-Hartmann <raphael.danilov@gmail.com>
2025-11-14 11:41:23 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-# Your first line of Python code
 import json
 
-def handle(input_data):
-    input_data = json.loads(input_data)
-    
-    return json.dumps(input_data)
+def handle(data):
+    data = json.loads(data)
+    data = json.dumps({"data": data})
+
+    return data
