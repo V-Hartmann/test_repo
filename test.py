6c321e936232a5b2bed05c0510a04702c0fff3ef
V-Hartmann <raphael.danilov@gmail.com>
2025-11-14 11:17:50 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import json
 def handle(n):
     empty_list = []
     a, b = 0, 1
-    while a < n:
+    while a < 1000:
         a, b = b, a+b
         empty_list.append([a, b])
-    return json.dumps({"list":empty_list})
+    return json.dumps({"list":empty_list, "input": n})
