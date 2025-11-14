f61523c66db9f628498202232feead7630a1cc01
V-Hartmann <raphael.danilov@gmail.com>
2025-11-14 16:02:03 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,42 @@
 import json
+from functools import reduce
 
 def handle(data):
     data = json.loads(data)
-    data = json.dumps({"data": data})
+    result = {}
+    for row in data["array_of_store"]:
+      if row["region"] in result:
+        result[row["region"]]["count"] += 1
+      else:
+        result[row["region"]] = {
+          "count": 1,
+          "child":{}
+        }
+    for row in data["array_of_store"]:
+      if row["region2"] in result[row["region"]]["child"]:
+        result[row["region"]]["child"][row["region2"]]["count"] += 1
+        result[row["region"]]["child"][row["region2"]]["child"].append(row["address"])
+      else:
+        result[row["region"]]["child"][row["region2"]] = {
+          "count": 1,
+          "child":[row["address"]]
+        }
 
-    return data
+    if data["store_stage"] == 1:
+      buttons = list(result.keys())
+    elif data["store_stage"] == 2:
+      input = data["input"]
+      item = result[input]
+      if item["count"] == 1:
+        item = next(iter(item["child"]))
+        if len(item["child"]) == 1:
+          buttons = []
+        else:
+          buttons = list(item["child"].keys())
+      else:
+        buttons = list(item["child"].keys())
+    elif data["store_stage"] == 3:
+      input = data["input"]
+
+    return json.dumps({"buttons": buttons}, ensure_ascii=False)
+handle(json_data)
